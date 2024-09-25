1d57aeafd756729f1ade02b96732f4d34256f45c
Joshua <97928637+jgx02c@users.noreply.github.com>
2024-09-25 16:55:46 -0700

    rgb removal

--- src/libs/Visualizer.js
+++ src/libs/Visualizer.js
@@ -77,8 +77,6 @@ const Visualizer = {
       for(let i = 0; i < bufferLength; i++) {
         barHeight = dataArray[i];
 
-        const rgb = self.hexToRgb(strokeColor);
-
         // canvasCtx.fillStyle = `rgb(${barHeight+100},${rgb.g},${rgb.b})`;
         canvasCtx.fillStyle = strokeColor;
         canvasCtx.fillRect(x,height-barHeight/2,barWidth,barHeight/2);
@@ -133,16 +131,6 @@ const Visualizer = {
     };
     draw();
   },
-
-
-  hexToRgb(hex) {
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : null;
-  }
 
 }
 
